1827b3fb2598a460110f1ccb084ded0cd4e26866
Rodrigo Vasco <r.vasco@hotmail.com>
2019-05-07 16:16:47 -0300

    Iniciando template do painel administrativo

--- template/admin.tpl.php
+++ template/admin.tpl.php
@@ -11,7 +11,34 @@
     <title>Painel Administrativo</title>
 </head>
 <body>
-<?php include $content ?>
+<div id="header">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a href="" class="navbar-brand">Admin</a>
+        <span class="navbar-text">
+            Painel Administrativo
+        </span>
+    </nav>
+</div>
+<div id="main">
+    <div class="row">
+        <div class="col">
+            <ul class="nav flex-column nav-pills bg-secondary text-white p-2">
+                <li class="nav-item">
+                    <span class="nav-link text-white-50"><small>MENU</small></span>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link active">Páginas</a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">Usuários</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-10">
+            <?php include $content ?>
+        </div>
+    </div>
+</div>
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
