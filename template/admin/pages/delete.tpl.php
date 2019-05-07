a87e135e01d02aafcb28304d41dee5eadfccede0
Rodrigo Vasco <r.vasco@hotmail.com>
2019-05-07 17:28:35 -0300

    Definindo rotas de administração de páginas

---
+++ template/admin/pages/delete.tpl.php
@@ -0,0 +1,5 @@
+<h3>Administração de páginas</h3>
+
+//Remoção de páginas
+
+<a href="/admin">Home</a>
---
+++ template/admin/pages/edit.tpl.php
@@ -0,0 +1,5 @@
+<h3>Administração de páginas</h3>
+
+//Edição de páginas
+
+<a href="/admin">Home</a>
