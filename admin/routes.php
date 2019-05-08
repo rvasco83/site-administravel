f21e7b84178612cda367fdd33244232690113956
Rodrigo Vasco <r.vasco@hotmail.com>
2019-05-08 17:26:20 -0300

    Criando funções para integrar com o banco de dados

--- admin/routes.php
+++ admin/routes.php
@@ -3,20 +3,8 @@
 if (resolve('/admin')) {
     render('admin/home', 'admin');
 
-} elseif (resolve('/admin/pages')) {
-    render('admin/pages/index', 'admin');
-
-} elseif (resolve('/admin/pages/create')) {
-    render('admin/pages/create', 'admin');
-
-} elseif (resolve('/admin/pages/(\d)+')) {
-    render('admin/pages/view', 'admin');
-
-} elseif (resolve('/admin/pages/(\d)+/edit')) {
-    render('admin/pages/edit', 'admin');
-
-} elseif (resolve('/admin/pages/(\d)+/delete')) {
-    render('admin/pages/delete', 'admin');
+} elseif (resolve('/admin/pages.*')) {
+    include __DIR__ . '/pages/routes.php';
 
 } else {
     http_response_code(404);
