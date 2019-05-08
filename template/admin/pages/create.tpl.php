91465a291e3604240fc44a129d02ca207ae795b9
Rodrigo Vasco <r.vasco@hotmail.com>
2019-05-08 12:00:33 -0300

    Template de formulário de cadastro de página

--- template/admin/pages/create.tpl.php
+++ template/admin/pages/create.tpl.php
@@ -1,5 +1,28 @@
 <h3>Administração de páginas</h3>
 
-//Criação de páginas
+<form action="" method="POST">
+    <div class="form-group">
+        <label for="pagesTitle">Título</label>
+        <input name="title" id="pagesTitle" type="text" class="form-control" placeholder="Digite o título da página">
+    </div>
 
-<a href="/admin">Home</a>
+    <div class="form-group">
+        <label for="pagesUrl">URL</label>
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <span class="input-group-text">/</span>
+            </div>
+            <input name="url" id="pagesUrl" type="text" class="form-control" placeholder="URL amigável, digite a url da página">
+        </div>
+    </div>
+
+    <div class="form-group">
+        // editor de texto
+    </div>
+
+    <button type="submit" class="btn btn-primary">Salvar</button>
+</form>
+
+<hr>
+
+<a href="/admin/pages" class="btn btn-secondary">Voltar</a>
