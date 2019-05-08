04f6c0a05d102e69c5024824fd878953d225d172
Rodrigo Vasco <r.vasco@hotmail.com>
2019-05-08 13:26:03 -0300

    Visualização de detalhes de página

--- template/admin/pages/view.tpl.php
+++ template/admin/pages/view.tpl.php
@@ -1,5 +1,33 @@
-<h3>Administração de páginas</h3>
+<h3 class="mb-5">Administração de páginas</h3>
 
-//Visualização
+<div class="row">
+    <div class="col-3">
+        <dl class="row">
+            <dt class="col-sm-4">Título</dt>
+            <dd class="col-sm-8">Página Inicial</dd>
 
-<a href="/admin">Home</a>
+            <dt class="col-sm-4">URL</dt>
+            <dd class="col-sm-8">/ - <a href="" target="_blank">Abrir</a></dd>
+
+            <dt class="col-sm-4">Criado:</dt>
+            <dd class="col-sm-8">2018-01-01 01:01</dd>
+
+            <dt class="col-sm-4">Atualizado:</dt>
+            <dd class="col-sm-8">2018-01-01 01:01</dd>
+        </dl>
+    </div>
+
+    <div class="col bg-light">
+        <h3>Página Inicial</h3>
+        <p>Está é a página inicial do site</p>
+    </div>
+</div>
+
+<p>
+    <a href="/admin/pages/1/edit" class="btn btn-primary">Editar</a>
+    <a href="/admin/pages/1/delete" class="btn btn-danger">Remover</a>
+</p>
+
+<hr>
+
+<a href="/admin/pages" class="btn btn-secondary">Voltar</a>
