ee59102d29bf9dbd5c98bc8ed124bd245b151c5b
Rodrigo Vasco <r.vasco@hotmail.com>
2019-05-10 11:49:12 -0300

    Cadastrando registro

--- admin/pages/db.php
+++ admin/pages/db.php
@@ -1,5 +1,18 @@
 <?php
 
+function pages_get_data ($redirectOnError) {
+    $title = filter_input(INPUT_POST,'title');
+    $url = filter_input(INPUT_POST, 'url');
+    $body = filter_input(INPUT_POST, 'body');
+
+    if (is_null($title) or is_null($url)) {
+        flash('Informe os campos de título e url', 'error');
+        header('location ' . $redirectOnError);
+    }
+
+    return compact('title', 'url', 'body');
+}
+
 $pages_all = function () use ($conn) {
     $result = $conn->query('SELECT * FROM pages');
     return $result->fetch_all(MYSQLI_ASSOC);
@@ -9,9 +22,17 @@ $pages_one = function ($id) {
     //buscar uma única página
 };
 
-$pages_create = function () {
-    //cadastrar uma página
+$pages_create = function () use ($conn) {
+    $data = pages_get_data('/admin/pages/create');
+
+    $sql = 'INSERT INTO pages (title, url, body, created, updated) VALUES (?,?,?, NOW(), NOW())';
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('sss', $data['title'], $data['url'], $data['body']);
+
     flash('Criou registro com sucesso', 'success');
+
+    return $stmt->execute();
 };
 
 $pages_edit = function ($id) {
