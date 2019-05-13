e4a31d5cae12b62d96c7bffa937f9c64542ad058
Rodrigo Vasco <r.vasco@hotmail.com>
2019-05-13 17:15:32 -0300

    Template de listagem de usuários

--- template/admin/users/index.tpl.php
+++ template/admin/users/index.tpl.php
@@ -1,1 +1,20 @@
-//lista users
+<h3 class="mb-5">Administração de usuários</h3>
+
+<table class="table table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>email</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <td>1</td>
+        <td>vasco@vasco.com</td>
+        <td class="text-right">
+            <a href="" class="btn btn-primary btn-sm">Ver</a>
+        </td>
+    </tbody>
+</table>
+
+<a href="/admin/users/create" class="btn btn-secondary">Novo</a>
